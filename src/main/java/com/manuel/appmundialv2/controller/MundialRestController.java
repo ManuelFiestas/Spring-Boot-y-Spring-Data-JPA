505ec7bea2e8fd202249075432931b9b744300dc
Witzy <mmfiestasmori@gmail.com>
2018-03-04 00:04:52 -0500

    probado back y front  en el mismo ide

--- src/main/java/com/manuel/appmundialv2/controller/MundialRestController.java
+++ src/main/java/com/manuel/appmundialv2/controller/MundialRestController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin("localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class MundialRestController {
